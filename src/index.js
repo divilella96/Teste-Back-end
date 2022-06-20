3a48d55e1111186abd6544e66fae2f6e66fcbb15
divilella96 <divilella@gmail.com>
2022-06-20 18:37:22 -0300

    configurando mongoDB

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+// configuração do banco 
+const port = process.env.PORT || 3030;
+const database_url = `${process.env.DATABASE_URL}${process.env.DATABASE_DB}` || 'mongodb://localhost:27017/';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.listen(port);
+
+// inserir rota aqui. 
+
+
+// conectando ao banco de dados
+mongoose.connect(database_url);
+
+
+
+
+module.exports = app
